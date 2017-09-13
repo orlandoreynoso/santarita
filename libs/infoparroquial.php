40b0b4fd87b42fa05c3b8b12c7b242829dcb2750
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-09-13 07:36:54 -0600

    size img

--- libs/infoparroquial.php
+++ libs/infoparroquial.php
@@ -4,13 +4,13 @@
 <div class="c-info" id="iglesia">
 	<div id="div1">
 		<a href="<?php bloginfo('url'); ?>/horarios-de-misa">
-			<img src="<?php echo IMAGES.'/jesus.jpg'; ?>" alt="">
+			<img src="<?php echo IMAGES.'/horario-de-misa.jpg'; ?>" alt="">
 			<span>Horarios de misa</span>
 		</a>
 	</div>
 	<div id="div1">
 		<a href="<?php bloginfo('url'); ?>/sacramentos/">
-			<img src="<?php echo IMAGES.'/los-sacramentos.jpg'; ?>" alt="">
+			<img src="<?php echo IMAGES.'/sacramentos.jpg'; ?>" alt="">
 			<span>Sacramentos</span>
 		</a>
 	</div>
@@ -18,6 +18,12 @@
 		<a href="<?php bloginfo('url'); ?>/parroquia-en-marcha/">
 			<img src="<?php echo IMAGES.'/santa-rita.jpg'; ?>" alt="">
 			<span>Parroquia en marcha</span>
+		</a>
+	</div>
+	<div id="div1">
+		<a href="<?php bloginfo('url'); ?>/calendario-de-eventos/">
+			<img src="<?php echo IMAGES.'/calendario.jpg'; ?>" alt="">
+			<span>Calendario de Eventos</span>
 		</a>
 	</div>
 	<!-- div id="div1">
